dfb93667fb1ee45db5508cc2300c07805a052a67
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-03-10 22:44:10 +0000

    chaneges

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # antd-second-components
+
+
+1111
+
+http://rexxars.github.io/react-markdown/
+var Markdown = require('react-markdown');
+ <Markdown source="# Your markdown here" />,
+
+ https://ant.design/components/steps/
+ 
---
+++ code.js
@@ -0,0 +1,94 @@
+import { Steps, Button, message } from 'antd';
+
+const Step = Steps.Step;
+
+const steps = [{
+  title: 'First',
+  content: 'First-content',
+}, {
+  title: 'Second',
+  content: 'Second-content',
+}, {
+  title: 'Last',
+  content: 'Last-content',
+}];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current: 0,
+    };
+  }
+
+  next() {
+    const current = this.state.current + 1;
+    this.setState({ current });
+  }
+
+  prev() {
+    const current = this.state.current - 1;
+    this.setState({ current });
+  }
+
+  render() {
+    const { current } = this.state;
+    return (
+      <div>
+        <Steps current={current}>
+          {steps.map(item => <Step key={item.title} title={item.title} />)}
+        </Steps>
+        <div className="steps-content">{steps[current].content}</div>
+        <div className="steps-action">
+          {
+            current < steps.length - 1
+            && <Button type="primary" onClick={() => this.next()}>Next</Button>
+          }
+          {
+            current === steps.length - 1
+            && <Button type="primary" onClick={() => message.success('Processing complete!')}>Done</Button>
+          }
+          {
+            current > 0
+            && (
+            <Button style={{ marginLeft: 8 }} onClick={() => this.prev()}>
+              Previous
+            </Button>
+            )
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, mountNode);
+
+----------
+import { Steps } from 'antd';
+
+const Step = Steps.Step;
+
+ReactDOM.render(
+  <Steps size="small" current={1}>
+    <Step title="Finished" />
+    <Step title="In Progress" />
+    <Step title="Waiting" />
+  </Steps>,
+  mountNode
+);
+
+-----------
+
+import { Steps } from 'antd';
+
+const Step = Steps.Step;
+
+ReactDOM.render(
+  <Steps direction="vertical" size="small" current={1}>
+    <Step title="Finished" description="This is a description." />
+    <Step title="In Progress" description="This is a description." />
+    <Step title="Waiting" description="This is a description." />
+  </Steps>,
+  mountNode
+);
