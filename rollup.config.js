4c705742a845c3bd82cdaaa6f9e1a342a75e2455
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-03-17 04:11:36 +0200

    1

--- rollup.config.js
+++ rollup.config.js
@@ -4,3 +4,69 @@ import json from 'rollup-plugin-json'
 import copy from 'rollup-plugin-copy-assets';
 import babel from 'rollup-plugin-babel'
 import pkg from './package.json'
+
+
+const extensions = [
+  '.js'
+]
+
+const name = 'AntDSecondSetComponents'
+
+
+export default {
+  input: './src/index.js',
+
+  // Specify here external modules which you don't want to include in your bundle (for instance: 'lodash', 'moment' etc.)
+  // https://rollupjs.org/guide/en#external-e-external
+  external: ['antd', 'lodash', 'uuid'],
+
+  plugins: [
+    // Allows node_modules resolution
+    resolve({ extensions }),
+
+    // Allow bundling cjs modules. Rollup doesn't understand cjs
+    commonjs(),
+
+    // Compile TypeScript/JavaScript files
+    babel({
+      extensions,
+      include: ['src/*'],
+      // include: ['src/**/*'],
+      exclude: [
+        'node_modules/**',
+        '/src/data/__tests__'
+      ]
+      // exclude: 'node_modules/**'
+      // presets: presets,
+      // plugins: plugins
+    }),
+
+    // Allow Rollup to import data from JSON file
+    // json()
+    // json({
+    //   include: '/src/data/**'
+    // }),
+
+    // juck fix in order to move json files to npm
+    copy({
+      assets: [
+        './src/data'
+      ],
+    }),
+  ],
+
+  output: [{
+    file: pkg.main,
+    format: 'cjs'
+  }, {
+    file: pkg.module,
+    format: 'es'
+  }, {
+    file: pkg.browser,
+    format: 'iife',
+    name,
+
+    // https://rollupjs.org/guide/en#output-globals-g-globals
+    globals: {}
+  }]
+}
